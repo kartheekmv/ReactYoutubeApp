951fe7301e2bd89e73d680408e1a09522a04d859
kartheekmv <you@example.com>
2018-03-31 12:47:12 -0500

    Added comments

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 
 // Create a new component. This component should produce some HTML
 
+// components in this case "App" is like a class? and we can use instances of thet using this syntax
+// <App></App>
+// since there is nothing between the tags we can write it as <App /> it is a valid JSX
 const App = () => {        // "=>"(fat arrow) almost same as function(){}
     return <div>Hi!</div>;
 }
